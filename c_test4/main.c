39936ed41564fe9e16c3c2dd3ccaf1ed903e69e2
Kazuki Oomizo <kazuki.oomizo123@gmail.com>
2014-08-14 09:56:21 +0900

    Initial Commit

---
+++ c_test4/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  c_test4
+//
+//  Created by kazu on 8/14/14.
+//  Copyright (c) 2014 KazukiOomizo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
